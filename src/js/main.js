449f4466ae86a0a3d8f748942d80050f8c11caaf
Denis <shotiqqq@gmail.com>
2022-09-22 20:41:58 +1000

    1920 with pp, 1024 and 768 without pp

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+          // Координаты центра карты.
+          // Порядок по умолчанию: «широта, долгота».
+          // Чтобы не определять координаты центра карты вручную,
+          // воспользуйтесь инструментом Определение координат.
+          center: [55.76761544581128, 37.63725550180111],
+          // Уровень масштабирования. Допустимые значения:
+          // от 0 (весь мир) до 19.
+          zoom: 16,
+        },
+        {
+          suppressMapOpenBlock: true,
+          geolocationControlSize: "large",
+          zoomControlSize: "small",
+          zoomControlFloat: "none",
+          zoomControlPosition: "none"
+        }
+      );
+
+      myMap.behaviors.disable('scrollZoom');
+
+      var myPlacemark = new ymaps.Placemark([55.76851434252156, 37.64130100359953], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/map_mark.svg',
+        iconImageSize: [12, 12],
+      })
+
+      const closeData = document.querySelector('.contacts__close');
+      const dataBlock = document.querySelector('.contacts__data');
+
+      closeData.addEventListener('click', () => {
+        dataBlock.classList.remove('contacts__data-visibility');
+      })
+
+      myPlacemark.events.add('click', function () {
+        dataBlock.classList.add('contacts__data-visibility');
+    });
+
+      myMap.geoObjects.add(myPlacemark);
+      myMap.controls.remove('trafficControl');
+      myMap.controls.remove('typeSelector');
+      myMap.controls.remove('fullscreenControl');
+      myMap.controls.remove('searchControl');
+      myMap.controls.remove('zoomControl');
+      myMap.controls.remove('rulerControl');
+      myMap.controls.remove('geolocationControl');
+    }
+
+
+})
